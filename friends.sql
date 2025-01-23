c34dffe7c36ac2cfe6f541cbed3a07e129d6ecee
TruwanThayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-01-23 09:10:16 +0000

    Create friends.sql

---
+++ friends.sql
@@ -0,0 +1,18 @@
+DROP TABLE iF EXISTS Friends;
+CREATE TABLE Friends(ID INT, FriendName TEXT, Gender TEXT);
+
+INSERT INTO Friends VALUES(1, 'Fake', 'M');
+INSERT INTO Friends VALUES(2, 'Name', 'M');
+INSERT INTO Friends VALUES(3, 'Not', 'F');
+INSERT INTO Friends VALUES(4, 'Real', 'F');
+INSERT INTO Friends VALUES(5, 'Ok', 'M');
+
+UPDATE Friends
+SET FriendName = 'Unreal', Gender = 'M'
+WHERE ID = 4;
+
+DELETE FROM Friends
+WHERE ID = 2;
+
+SELECT * FROM Friends;
+
