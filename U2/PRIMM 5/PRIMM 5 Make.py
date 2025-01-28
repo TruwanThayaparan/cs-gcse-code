de2ff36f9a3eea64855365209beb520220cb5e59
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-01-28 21:20:49 +0000

    :( the faces r gone oh no!!

--- U2/PRIMM 5/PRIMM 5 Make.py
+++ U2/PRIMM 5/PRIMM 5 Make.py
@@ -1,6 +1,6 @@
 var = input("Input something...")
 
 if len(var) < 10:
-    print("Shorter than 10 characters :)")
+    print("The string is shorter than 10 characters!")
 else:
-    print("Longer than 10 characters :(")
+    print("The string is longer than 10 characters!")
