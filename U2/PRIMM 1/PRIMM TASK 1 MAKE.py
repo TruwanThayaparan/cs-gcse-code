5a2291232039491266c7f707907b5fbbcf9f5a14
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-01-28 21:27:01 +0000

    Update PRIMM TASK 1 MAKE.py

--- U2/PRIMM 1/PRIMM TASK 1 MAKE.py
+++ U2/PRIMM 1/PRIMM TASK 1 MAKE.py
@@ -9,12 +9,12 @@ q1 = input("What's your name, very nice person?") # provide name
 
 # Modify task
 import random # use random module to provide you a random complement
-random_number_wah = random.randint(1,3)
+random_number = random.randint(1,3)
 
-if random_number_wah == 1:
+if random_number == 1:
      print(q1 + ", you're amazing!") # original complement
-elif random_number_wah == 2:
+elif random_number == 2:
      print(q1 + ", you're wonderful!") # alternative 
-else: # if random_number_wah is 3
+else: # if random_number is 3
      print(q1 + ", you're awesome!") # alternative
 
