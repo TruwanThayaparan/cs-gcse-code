2ed392626e90958247288ff1bd9d188529f6e1d1
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-02-22 21:30:39 +0000

    Create fizzbuzz.lua

---
+++ U2/fizzbuzz.lua
@@ -0,0 +1,15 @@
+-- FizzBuzz.py
+-- Print all numbers from 1 to the finishing number (100)
+-- But numbers divisible by 3 print Fizz instead, numbers divisible by 5 print Buzz and numbers divisible by 3 and 5 print FizzBuzz.
+
+for i = 1, 100 do
+    if i % 3 == 0 and i % 5 == 0 then
+        print("FizzBuzz")
+    elseif i % 3 == 0 then
+        print("Fizz")
+    elseif i % 5 == 0 then
+        print("Buzz")
+    else
+        print(i)
+    end
+end
