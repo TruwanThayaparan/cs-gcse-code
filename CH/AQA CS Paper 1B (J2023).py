ceec0268e856e909a61b7a490cd9bb1f338df862
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-19 15:10:52 +0000

    Create AQA CS Paper 1B (J2023).py

---
+++ CH/AQA CS Paper 1B (J2023).py
@@ -0,0 +1,27 @@
+# final question
+import random
+score = 0
+while True:
+	dice1 = random.randint(1,6)
+	dice2 = random.randint(1,6)
+	total = dice1 + dice2
+	score += total
+	print(“Dice 1:”, dice1)
+	print(“Dice 2:”, dice2)
+	print(“Current score:”, score)
+	if score < 21:
+		continue = input(“Would you like to roll again?”)
+		If continue != “yes”:
+			break
+	else:
+		break
+
+if score == 21:
+	print(“You won!”)
+elif score > 21:
+	print(“You lost!”)
+else:
+	if random.randint(15, 21) > score:
+		print(“You lost!”)
+	else:
+		print(“You won!”)
