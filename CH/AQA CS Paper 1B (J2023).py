59881b9632f202b14513fcfaf54aea003eb93152
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-19 15:11:49 +0000

    yucky 66 99 ones removed

--- CH/AQA CS Paper 1B (J2023).py
+++ CH/AQA CS Paper 1B (J2023).py
@@ -6,22 +6,22 @@ while True:
 	dice2 = random.randint(1,6)
 	total = dice1 + dice2
 	score += total
-	print(“Dice 1:”, dice1)
-	print(“Dice 2:”, dice2)
-	print(“Current score:”, score)
+	print("Dice 1:", dice1)
+	print("Dice 2:", dice2)
+	print("Current score:", score)
 	if score < 21:
-		continue = input(“Would you like to roll again?”)
-		If continue != “yes”:
+		continue = input("Would you like to roll again?")
+		If continue != "yes":
 			break
 	else:
 		break
 
 if score == 21:
-	print(“You won!”)
+	print("You won!")
 elif score > 21:
-	print(“You lost!”)
+	print("You lost!")
 else:
 	if random.randint(15, 21) > score:
-		print(“You lost!”)
+		print("You lost!")
 	else:
-		print(“You won!”)
+		print("You won!")
