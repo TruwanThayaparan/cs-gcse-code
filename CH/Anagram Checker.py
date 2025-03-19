eff79718869c9e648028920d80aa00c6802f0c84
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-19 17:32:19 +0000

    Create Anagram Checker.py

---
+++ CH/Anagram Checker.py
@@ -0,0 +1,22 @@
+word = input("Input a word: ")
+word2 = input("Input another word: ")
+checked = []
+correct = 0
+if len(word) == len(word2):
+  for i in range(len(word)):
+    for j in range(len(word)):
+        print(i, j, word[i], word2[j])
+        if word[i] == word2[j]:
+            if j not in checked:
+                correct += 1
+                checked.append(j)
+                print(correct)
+                break
+            
+    
+  if correct == len(word):
+        print("Anagramed.")
+  else:
+        print("Not anagramed.")
+else:
+    print("Not anagramed.")
