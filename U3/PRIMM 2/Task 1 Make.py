5d3fcb6627b1557f6e278edbe42959e7904e0b76
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-27 09:51:36 +0000

    Create Task 1 Make.py

---
+++ U3/PRIMM 2/Task 1 Make.py
@@ -0,0 +1,6 @@
+ClassMates = [["Generic Person A", "GPB", "GPC"], ["Random Person A", "RPB", "RPC"], ["Some Person A", "SPB", "SPC"]]
+print(ClassMates[0][1])
+print(ClassMates[1][0])
+print(len(ClassMates))
+ClassMates.insert([3][0], ["A Person 1", "AP2", "AP3"])
+print(ClassMates)
