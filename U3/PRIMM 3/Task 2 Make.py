ade2f9252a92e3dc5f1b6733485115cba2e12c37
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-28 13:11:15 +0000

    Create Task 2 Make.py

---
+++ U3/PRIMM 3/Task 2 Make.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+Films = namedtuple('films', 'film_name genre age_rating year_of_release')
+
+Film1 = Films(
+Film2 = Films(
+Film3 = Films(
+Film4 = Films(
+Film5 = Films(
+
+print(Film1)
+print(Film2)
+print(Film3)
+print(Film4)
+print(Film5)
