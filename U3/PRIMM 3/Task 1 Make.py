de77262cbf3fb4750069d14f1f9da1dc70a78eb2
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-28 13:06:11 +0000

    Create Task 1 Make.py

---
+++ U3/PRIMM 3/Task 1 Make.py
@@ -0,0 +1,35 @@
+Book1 = ()
+Book2 = ()
+Book3 = ()
+Book4 = ()
+Book5 = ()
+
+print("-"*30)
+
+print(Book1)
+print(len(Book1))
+
+print(Book2)
+print(len(Book2))
+
+print(Book3)
+print(len(Book3))
+
+print(Book4)
+print(len(Book4))
+
+print(Book5)
+print(len(Book5))
+
+print("-"*30)
+
+(key,value1,value2,value3) = Book1
+(key,value1,value2,value3) = Book2
+(key,value1,value2,value3) = Book3
+(key,value1,value2,value3) = Book4
+(key,value1,value2,value3) = Book5
+
+print(key)
+print(value1)
+print(value2)
+print(value3)
