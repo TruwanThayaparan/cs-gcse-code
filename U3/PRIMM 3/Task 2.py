2c6b2adb8714c05a270268e262919f41a8b37d03
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-28 13:08:08 +0000

    Create Task 2.py

---
+++ U3/PRIMM 3/Task 2.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+
+Pets = namedtuple('peta', 'animal name age')
+
+Dottie = Pets(animal = 'cat', name = 'Dottie', age = 9)
+Jack = Pets(animal = 'cat', name = 'Jack', age = 9)
+Lassie = Pets(animal = 'dog', name = 'Lassie', age = 13)
+
+print(Dottie)
+print(Jack)
+print(Lassie)
