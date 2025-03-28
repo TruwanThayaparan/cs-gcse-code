f2acfaae1535e2715bfa212fc2a2cca85c0d9416
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-28 13:13:11 +0000

    Update Task 1 Make.py

--- U3/PRIMM 3/Task 1 Make.py
+++ U3/PRIMM 3/Task 1 Make.py
@@ -1,10 +1,10 @@
-Book1 = ()
-Book2 = ()
-Book3 = ()
-Book4 = ()
-Book5 = ()
+Book1 = ("Book One", "Author One", 2020, "Fiction")
+Book2 = ("Book Two", "Author Two", 2021, "Non-Fiction")
+Book3 = ("Book Three", "Author Three", 2019, "Mystery")
+Book4 = ("Book Four", "Author Four", 2023, "Science Fiction")
+Book5 = ("Book Five", "Author Five", 2022, "Biography")
 
-print("-"*30)
+print("-" * 30)
 
 print(Book1)
 print(len(Book1))
@@ -21,15 +21,42 @@ print(len(Book4))
 print(Book5)
 print(len(Book5))
 
-print("-"*30)
+print("-" * 30)
 
-(key,value1,value2,value3) = Book1
-(key,value1,value2,value3) = Book2
-(key,value1,value2,value3) = Book3
-(key,value1,value2,value3) = Book4
-(key,value1,value2,value3) = Book5
+(key1, value1_1, value1_2, value1_3) = Book1
+(key2, value2_1, value2_2, value2_3) = Book2
+(key3, value3_1, value3_2, value3_3) = Book3
+(key4, value4_1, value4_2, value4_3) = Book4
+(key5, value5_1, value5_2, value5_3) = Book5
 
-print(key)
-print(value1)
-print(value2)
-print(value3)
+print("Unpacked values for Book1:")
+print(key1)
+print(value1_1)
+print(value1_2)
+print(value1_3)
+
+print("Unpacked values for Book2:")
+print(key2)
+print(value2_1)
+print(value2_2)
+print(value2_3)
+
+print("Unpacked values for Book3:")
+print(key3)
+print(value3_1)
+print(value3_2)
+print(value3_3)
+
+print("Unpacked values for Book4:")
+print(key4)
+print(value4_1)
+print(value4_2)
+print(value4_3)
+
+print("Unpacked values for Book5:")
+print(key5)
+print(value5_1)
+print(value5_2)
+print(value5_3)
+
+print("-" * 30)
