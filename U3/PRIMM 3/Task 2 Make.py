a3f6ddeb22fb53be5eeccbfcc0aad73c9c5bb403
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-28 13:15:00 +0000

    Update Task 2 Make.py

--- U3/PRIMM 3/Task 2 Make.py
+++ U3/PRIMM 3/Task 2 Make.py
@@ -2,11 +2,11 @@ from collections import namedtuple
 
 Films = namedtuple('films', 'film_name genre age_rating year_of_release')
 
-Film1 = Films(
-Film2 = Films(
-Film3 = Films(
-Film4 = Films(
-Film5 = Films(
+Film1 = Films('The Shawshank Redemption', 'Drama', 'R', 1994)
+Film2 = Films('Inception', 'Sci-Fi', 'PG-13', 2010)
+Film3 = Films('The Lion King', 'Animation', 'G', 1994)
+Film4 = Films('Pulp Fiction', 'Crime', 'R', 1994)
+Film5 = Films('Avatar', 'Sci-Fi', 'PG-13', 2009)
 
 print(Film1)
 print(Film2)
