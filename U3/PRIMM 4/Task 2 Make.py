de9d3c95d9b566d92cfa40ba2b188e67169daba3
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-31 10:23:36 +0100

    Create Task 2 Make.py

---
+++ U3/PRIMM 4/Task 2 Make.py
@@ -0,0 +1,25 @@
+citiesListRAW = []
+citiesListCLEAN = []
+filCities = open("cities.txt", "rt")
+for line in filCities:
+    citiesListRAW.append(line)
+
+filCities.close()
+for each in citiesListRAW:
+    if (each in citiesListCLEAN) == False:
+        citiesListCLEAN.append(each)
+
+# for element in citiesListClean: print(element)
+
+fileOut = open("unique.txt", "wt")
+for each in citiesListCLEAN:
+    fileOut.write(each)
+fileOut.close()
+
+fileOut = open("unique.txt", "a")
+fileOut.write("501, Watford \n")
+fileOut.close()
+
+import random
+randomCityID = random.randint(0, len(citiesListCLEAN))
+print(citiesListCLEAN[randomCityID])
