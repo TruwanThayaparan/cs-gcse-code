128ddb19431e9cf9d261b74087deb848b69bbe59
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-31 10:12:07 +0100

    Update Task 1.py

--- U3/PRIMM 3/Task 1.py
+++ U3/PRIMM 3/Task 1.py
@@ -1,9 +1,12 @@
-Car1 = ("Ford", "Focus", "Blue")
+# Creating a tuple for cars
+
+Car1 = ("Ford", "Focus", "Blue") # Creates a 3 element tuple
 Car2 = ("Vauxhal", "Astra", "Red")
 Car3 = ("Jaguar", "XF", "Silver")
 
 print("-"*30)
 
+# Print out the elements and length of each tuple
 print(Car1)
 print(len(Car1))
 
@@ -15,7 +18,8 @@ print(len(Car3))
 
 print("-"*30)
 
-(key,value1,value2) = Car1
-print(key)
-print(value1)
-print(value2)
+# Tuples are simply groups of data and therefore can be unpacked
+(key,value1,value2) = Car1 # Defines the key and values of the tuple
+print(key)      # Prints the key of the tuple
+print(value1)   # Prints the 1st value of the tuple
+print(value2)   # Prints the 2nd value of the tuple
