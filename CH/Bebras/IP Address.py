1fa237698502fcb1e45980064829cf9c87f6f15b
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-31 20:50:38 +0100

    bebras is finally right

--- CH/Bebras/IP Address.py
+++ CH/Bebras/IP Address.py
@@ -1,17 +1,16 @@
-# bebras is wrong again
 ip_address = input()
 split_it_up = ip_address.split(".")
 validIP = True
-print(split_it_up)
-print(len(split_it_up))
+
 if len(split_it_up) == 4:
     for i in range(len(split_it_up)):
-        if not 0 <= int(split_it_up[i]) <= 255:
+        if not split_it_up[i].isdigit() or not 0 <= int(split_it_up[i]) <= 255:
             validIP = False
-            print("Invalid IP address"); break
+            break
 else:
     validIP = False
-    print("Invalid IP address")
 
 if validIP:
     print("Valid IP address")
+else:
+    print("Invalid IP address")
