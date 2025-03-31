58e2825844ffe35d253cb26ad2905dd7bccf2311
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-03-31 10:13:02 +0100

    Create Task 1 Modify.py

---
+++ U3/PRIMM 4/Task 1 Modify.py
@@ -0,0 +1,15 @@
+myFile = open("example.txt", "wt")
+myFile.write("I have written to a file. \n")
+myFile.write("It now has 3 lines. \n")
+myFile.write("The third being this one. \n")
+myFile.close()
+
+myFile = open("example.txt", "a")
+myFile.write("PS: Here is another line I forgot to add! \n")
+myFile.write("PS 2: My name is Truwan. \n")
+myFile.close()
+
+myFile = open("example.txt", "rt")
+contents = myFile.read()
+print(contents)
+myFile.close()
