41911c4deb443a1810ba4b9d6038df3b0a0b83a0
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-01 20:14:09 +0100

    Update Anagram Checker.py

--- CH/Other/Anagram Checker.py
+++ CH/Other/Anagram Checker.py
@@ -5,7 +5,7 @@ correct = 0
 if len(word) == len(word2):
   for i in range(len(word)):
     for j in range(len(word)):
-        print(i, j, word[i], word2[j])
+        # print(i, j, word[i], word2[j])
         if word[i] == word2[j]:
             if j not in checked:
                 correct += 1
