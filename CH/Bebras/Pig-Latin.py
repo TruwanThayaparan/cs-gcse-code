69b72e91955b3b7edcf2db137e07e61afd54489a
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-01 20:09:59 +0100

    my bad i didn't read it

--- CH/Bebras/Pig-Latin.py
+++ CH/Bebras/Pig-Latin.py
@@ -1,6 +1,4 @@
-# bebras erronously marks this as 0/4 marks but I can tell you it should be full marks
-
-word = input("Enter a word.")
+word = input()
 if word[0].lower() not in ["a", "e", "i", "o", "u"]:
     word_keep = word[0]
     word = word[1:]
