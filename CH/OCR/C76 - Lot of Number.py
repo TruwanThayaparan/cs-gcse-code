371498b8711e1e6875d33a65a1c323def48d7ce9
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-28 10:32:34 +0100

    Create C76 - Lot of Number.py

---
+++ CH/OCR/C76 - Lot of Number.py
@@ -0,0 +1,19 @@
+# Challenge 76 - That’s a lot of number
+
+# text_file = open('numbers.txt','r')
+numbers_to_add = []
+
+lines = text_file.readlines()
+
+for line in lines:
+    # print(line)
+    lint = int(line)
+    numbers_to_add.append(lint)
+
+# print(numbers_to_add)
+
+answer = 0 
+for i in range(len(numbers_to_add)):
+    answer += numbers_to_add[i]
+
+print(answer)
