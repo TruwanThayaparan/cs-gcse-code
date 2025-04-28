16de3966a7ef9df7e71dabffa4b53c083214b278
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-28 19:47:29 +0100

    why is there a dupe?!??!

---
+++ CH/OCR/C45 - Find the Factorial.py
@@ -0,0 +1,13 @@
+# Find the Factorial [Challenge 45]
+
+n = int(input("Enter a number... "))
+
+if n < 0:
+    print("Factorial is not defined for negative numbers.")
+elif n == 0:
+    print("1")
+else:
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    print(result)
