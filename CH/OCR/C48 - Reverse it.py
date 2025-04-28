4495e4610fd7cadea382b616dc7ad96a8db13766
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-28 22:09:31 +0100

    Update C48 - Reverse it.py

--- CH/OCR/C48 - Reverse it.py
+++ CH/OCR/C48 - Reverse it.py
@@ -1,1 +1,29 @@
-wt
+# Challenge 48 - Reverse it
+lets_generate = input("Enter your word: ")
+
+letters = []
+
+for i in range(len(lets_generate)):
+    letters.append(lets_generate[i])
+    
+# print(letters)
+
+letters.reverse()
+
+red = ""
+
+for i in range(len(letters)):
+    red += letters[i]
+    
+print(red)
+
+vowels = "aeiou"
+vx = 0
+for v in vowels:
+    vx += red.lower().count(v)
+    
+print("Length:",red)
+print("Vowels:",vx)
+print("Consonants:", len(red) - vx)
+
+# Palindrome Checker can easily be implemented from my Palindromes script
