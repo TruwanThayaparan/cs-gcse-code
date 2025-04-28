97960079a5c71c9499388f4cb04e50f84f83cd6b
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-28 21:58:06 +0100

    Create C38 - Sing Along.py

---
+++ CH/OCR/C38 - Sing Along.py
@@ -0,0 +1,16 @@
+# Challenge 38 - Sing Along
+def bottle_text(n): # function here makes the code look nicer
+    if n == 0:
+        return "no green bottles"
+    elif n == 1:
+        return "1 green bottle"
+    else:
+        return f"{n} green bottles"
+
+start = int(input("Where should we start singing '10 Green Bottles' from? "))
+
+for i in range(start, 0, -1):
+    print(f"{bottle_text(i)} hanging on the wall,")
+    print(f"{bottle_text(i)} hanging on the wall,")
+    print("And if one green bottle should accidentally fall,")
+    print(f"There'll be {bottle_text(i-1)} hanging on the wall.\n") # spacing
