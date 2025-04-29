274ec38bbac357a23f389edd8111ebb4a3c40f41
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-04-29 19:31:21 +0100

    Update C25 - Ordering.py

--- CH/OCR/C25 - Ordering.py
+++ CH/OCR/C25 - Ordering.py
@@ -1,1 +1,34 @@
 # Challenge 25 - Ordering
+numbers = []
+
+for i in range(10):
+    while True:
+        try:
+            print(i + 1)
+            num = int(input("Enter your number: "))
+            numbers.append(num)
+            break
+        except ValueError:
+            print("Please enter a number.")
+
+print(numbers)
+
+# Bubble sort (it's a short list)
+Passes = 0
+Swaps = 0
+hasSorted = True
+while hasSorted:
+    hasSorted = False
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i + 1]:
+                temp = numbers[i]
+                temp2 = numbers[i+1]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = temp
+                Swaps += 1
+                hasSorted = True
+        else:
+            continue
+    Passes += 1
+
+print(numbers)
