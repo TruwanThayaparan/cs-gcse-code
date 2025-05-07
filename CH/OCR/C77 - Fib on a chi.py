b48c2588106bb14b624fc44f5cd8f1f22f06e73e
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-07 11:45:28 +0100

    Create C77 - Fib on a chi.py

---
+++ CH/OCR/C77 - Fib on a chi.py
@@ -0,0 +1,17 @@
+# Challenge 77 - Fib on a chi
+fib_sequence = []
+previous_number = 0
+current_number = 1
+seq = 1
+print("Sequence:")
+while True:
+    fib_sequence.append(previous_number)
+    ##print(previous_number)
+    next_number = previous_number + current_number
+    previous_number = current_number
+    current_number = next_number
+    seq += 1
+    if len(str(current_number)) == 3:
+      print("The index and number of the first term in the Fibonacci sequence that contains 1,000 digits:")
+      print(seq, current_number)
+      break
