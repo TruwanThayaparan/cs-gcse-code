51d1fd8d9951d63413f3e4ce156656ca0ea314e9
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-30 13:38:23 +0100

    Fixed.

--- CH/OCR/C03 - Thief!.py
+++ CH/OCR/C03 - Thief!.py
@@ -1,5 +1,31 @@
 # Challenge 3 - Thief!
 
+pin = "4214" # set this to any 4-digit number
+dan = input("Input the four digits in question: ")
+numbers = [dan[0], dan[1], dan[2], dan[3]]
+saved = []
+print(numbers)
+
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            for l in range(4):
+                attempt = dan[i] + dan[j] + dan[k] + dan[l]
+                if len(set([dan[i], dan[j], dan[k], dan[l]])) < 4:
+                    continue
+                if attempt in saved:
+                    continue
+                if attempt == pin:
+                    print("PIN HAS BEEN CRACKED:", attempt)
+                else:
+                    print(attempt)
+                saved.append(attempt)
+
+
+# OLD VERSION (THIS DOES NOT ANSWER THE PROMPT CORRECTLY)
+'''
+# Challenge 3 - Thief!
+
 pin = "4214" # set this to any 4 digit number
 dan = input("Input the four digits in question: ")
 numbers = [dan[0], dan[1], dan[2], dan[3]]
@@ -29,3 +55,4 @@ for i in range(4):
             break
     if found:
         break
+'''
