8c04fbdab50ff95b684d0849e89ee88ff40a4dc5
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 12:10:22 +0100

    Update C13 - Caesar Cipher.py

--- CH/OCR/C13 - Caesar Cipher.py
+++ CH/OCR/C13 - Caesar Cipher.py
@@ -1,59 +1,49 @@
-# Challenge 13 - Caesar Cipher
-
 print("Welcome to the Caesar Cipher Encryptor/Decryptor!")
 
 def menu():
-  while True:
-    mode = input("\nDo you want to decrypt or encrypt a message? ").strip().lower()
-      
-    if mode not in ("decrypt", "encrypt"):
-        print(f"{mode} is not a valid mode. Please try again.")
-        continue
-    
-    break
-    
-  message = input(f"Enter the message you want to {mode}: ")
+    while True:
+        while True:
+            mode = input("\nDo you want to decrypt or encrypt a message? ").strip().lower()
+            if mode not in ("decrypt", "encrypt"):
+                print(f"{mode} is not a valid mode. Please try again.")
+                continue
+            break
+        
+        message = input(f"Enter the message you want to {mode}: ")
 
-  while True:
-    try:
-      key = int(input("Enter the amount of shifts for your message: ").strip())
-      if 1 <= key <= 25:
-        break
-      print("You must enter an integer between 1 and 25.")
-    except ValueError:
-      print("You must enter an integer.")
+        while True:
+            try:
+                key = int(input("Enter the amount of shifts for your message: ").strip())
+                if 1 <= key <= 25:
+                    break
+                print("You must enter an integer between 1 and 25.")
+            except ValueError:
+                print("You must enter an integer.")
 
-  final = run_cipher(message, key, mode)
-  print(f"\nThe {mode}ed message is: {final}.")
+        final = run_cipher(message, key, mode)
+        print(f"\nThe {mode}ed message is: {final}.")
 
-  keep_going = input("Do you want to decrypt/encrypt another message? ")
-  if keep_going.strip().lower() in ("no", "n"):
-      print("Script ended.")
-      return
-  else:
-      menu()
-    
+        keep_going = input("Do you want to decrypt/encrypt another message? (yes/no): ")
+        if keep_going.strip().lower() in ("no", "n"):
+            print("Script ended.")
+            break
+
 def run_cipher(message, key, mode):
     result = []
 
     for char in message:
         if char.isalpha():
             if char.isupper():
-              offset = 65 
+                offset = 65 
             else:
-              offset = 97
-              
-            if mode == "decrypt":
-              shift = -key 
-            else:
-              shift = key
-              
+                offset = 97
+                
+            shift = -key if mode == "decrypt" else key
             new_char = chr((ord(char) - offset + shift) % 26 + offset)
             result.append(new_char)
         else:
             result.append(char)
 
-    final = ''.join(result)
-    return final
+    return ''.join(result)
 
 menu()
