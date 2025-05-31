bc539d1a506736bb5a4cbb45c1c5f39c4ec81566
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 19:10:46 +0100

    SPAM SPAM SPAM SPAM SPAM SPAM SPAM

---
+++ CH/OCR/C65 - Spam filter.py
@@ -0,0 +1,34 @@
+# Challenge 65 - Spam filter
+import random
+
+dishes = [
+  "Eggs Benedict", 
+  "Pancakes with syrup", 
+  "Grilled cheese sandwich", 
+  "Caesar salad", 
+  "Tomato soup", 
+  "Roast chicken", 
+  "Fish and chips", 
+]
+
+spam_dishes = []
+
+for dish in dishes:
+    words = dish.split(' ')
+
+    new_words = []
+    for i, word in enumerate(words):
+        # Add the word
+        new_words.append(word)
+        if i < len(words) - 1 and random.choice([True, False]):
+            new_words.append("Spam")
+    
+    if random.choice([True, False]):
+        new_words.insert(0, "Spam")
+    if random.choice([True, False]):
+        new_words.append("Spam")
+    
+    spam_dishes.append(" ".join(new_words))
+
+for sd in spam_dishes:
+    print(sd)
