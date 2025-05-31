14cf1d3c08fac835e08ce84cf1402b4483245693
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 18:45:38 +0100

    Update C20 - Palindromes.py

--- CH/OCR/C20 - Palindromes.py
+++ CH/OCR/C20 - Palindromes.py
@@ -2,15 +2,13 @@
 
 pal = input("Enter a word into the palindrome checker... ")
 
-palidrome = True
-for i in range(len(pal) - 1):
-  if pal[i] == pal[len(pal) - 1 - i]:
-    continue
-  else:
-    palidrome = False
-    break
-    
-if palidrome:
+palindrome = True
+for i in range(len(pal) // 2): # Only check half the string
+    if pal[i] != pal[len(pal) - 1 - i]:
+        palindrome = False
+        break
+
+if palindrome:
     print(f"{pal} is a palindrome.")
 else:
     print(f"{pal} is not a palindrome.")
