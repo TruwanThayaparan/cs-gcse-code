c7314c834aa66999ad18f7a920603ff946556ad9
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 18:22:51 +0100

    minor fixes

--- CH/OCR/C05 - Fruit Machine.py
+++ CH/OCR/C05 - Fruit Machine.py
@@ -6,23 +6,24 @@ print("Welcome to the FRUIT MACHINE!")
        
 while True:
   while True:
-    print("You have", credits," credits!")
-    response = input("Would you like to roll the fruit machine for 20 credits? ").lower()
-    if response in ("yes", "no"):
+    print(f"You have {credits} credits!")
+    response = input("Would you like to roll the fruit machine for 20 credits? ").strip().lower()
+    if response in ("yes", "y", "no", "n"):
       break
     else:
       print("You must say yes or no.")
 
-  if response == "no": print("Goodbye!"); break
+  if response in ("no", "n"):
+       print("Goodbye!")
+       break
 
   credits -= 20
 
   fruit1, fruit2, fruit3 = random.choice(symbols), random.choice(symbols), random.choice(symbols)
-  
-  print(fruit1, fruit2, fruit3)
+  print(f"{fruit1} {fruit2} {fruit3}\n")
 
   if fruit1 == fruit2 or fruit2 == fruit3 or fruit1 == fruit3:
-    if fruit1 == fruit2 and fruit2 == fruit3:
+    if fruit1 == fruit2 == fruit3:
       if fruit1 == "Bell":
         print("You rolled the bell 3 times! You earn 500 credits!")
         credits += 500
@@ -37,10 +38,12 @@ while True:
       if skulls == 2:
         print("You rolled the skull 2 times! You lose 100 credits!")
         credits -= 100
+        credits = max(credits, 0)
       else:
         print("You rolled the same fruit 2 times! You earn 50 credits!")
         credits += 50
   
   if credits < 20:
-    print("Oh no, you do not have enough credits to re-roll (you only have", credits, "). Goodbye!")
+    credits = max(credits, 0)
+    print(f"Oh no, you do not have enough credits to re-roll (you only have {credits} credits). Goodbye!")
     break
