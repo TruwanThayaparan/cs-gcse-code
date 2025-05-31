831e5f8b2647c6621712f1622efd87e4051b976d
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 21:37:03 +0100

    Create C7 - Credit Card Validator.py

---
+++ CH/OCR/C7 - Credit Card Validator.py
@@ -0,0 +1,24 @@
+# Challenge 7 - Credit Card Validator
+
+credit_card_number = input("Please enter a credit card number into the CC validator: ").strip()
+
+check_digit = int(credit_card_number[-1])
+new_ccn = credit_card_number[:-1]
+
+new_ccn_reversed = new_ccn[::-1]
+
+processed_digits = []
+for idx, digit_char in enumerate(new_ccn_reversed):
+    digit = int(digit_char)
+    if idx % 2 == 0:
+        digit *= 2
+        if digit > 9:
+            digit -= 9
+    processed_digits.append(digit)
+
+z = sum(processed_digits) + check_digit
+
+if z % 10 == 0:
+    print("Card number is valid!")
+else:
+    print("Card number is invalid.")
