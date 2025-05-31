c202e7b325e51d925a8080a3af1233c22fcb9715
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 21:36:29 +0100

    Update C52 - Is this card valid.py

--- CH/OCR/C52 - Is this card valid.py
+++ CH/OCR/C52 - Is this card valid.py
@@ -2,31 +2,22 @@
 
 credit_card_number = input("Please enter a credit card number into the CC validator: ").strip()
 
-# credit_card_number = "79927398713"
 check_digit = int(credit_card_number[-1])
 new_ccn = credit_card_number[:-1]
-
-# print(check_digit)
-# print(new_ccn)
-# print(list(new_ccn))
 
 new_ccn_reversed = new_ccn[::-1]
 
 processed_digits = []
-x = 1
-for i in new_ccn_reversed:
-    i = int(i)
-    if x % 2 == 1:
-        i *= 2
-        if i > 9:
-            i -= 9
-    processed_digits.append(i)
-    x += 1
+for idx, digit_char in enumerate(new_ccn_reversed):
+    digit = int(digit_char)
+    if idx % 2 == 0:
+        digit *= 2
+        if digit > 9:
+            digit -= 9
+    processed_digits.append(digit)
 
-z = sum(processed_digits)
-# print(z)
+z = sum(processed_digits) + check_digit
 
-z += check_digit
 if z % 10 == 0:
     print("Card number is valid!")
 else:
