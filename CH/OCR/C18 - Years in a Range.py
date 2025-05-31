5756daf8bae9c0dcc16ea285d8162710f409738b
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-05-31 19:33:50 +0100

    e

--- CH/OCR/C18 - Years in a Range.py
+++ CH/OCR/C18 - Years in a Range.py
@@ -1,6 +1,18 @@
 # Challenge 18 - Years in a Range
-syear = int(input("Starting year: "))
-eyear = int(input("Ending year: "))
+
+while True:
+    try:
+        syear = int(input("Starting year: "))
+        break
+    except ValueError:
+        print("You must enter a number.")
+
+while True:
+    try:
+        eyear = int(input("Ending year: "))
+        break
+    except ValueError:
+        print("You must enter a number.")
 
 for year in range(syear, eyear + 1):
     year_str = str(year)
