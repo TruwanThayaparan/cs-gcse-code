21e64b6db9c4f5ed2a886815e94e6e6714ae92a1
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-06-01 12:44:34 +0100

    Create C10 - Check if Palindrome.py

---
+++ CH/OCR 20/C10 - Check if Palindrome.py
@@ -0,0 +1,14 @@
+# Challenge 10 - Check if Palindrome
+
+pal = input("Enter a word into the palindrome checker... ")
+
+palindrome = True
+for i in range(len(pal) // 2): # Only check half the string
+    if pal[i] != pal[len(pal) - 1 - i]:
+        palindrome = False
+        break
+
+if palindrome:
+    print(f"{pal} is a palindrome.")
+else:
+    print(f"{pal} is not a palindrome.")
