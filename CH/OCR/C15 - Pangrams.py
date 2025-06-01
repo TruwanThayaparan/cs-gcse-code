8ade31d81afcbda36177eee65fcf2295165423b7
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-06-01 17:41:08 +0100

    Update C15 - Pangrams.py

--- CH/OCR/C15 - Pangrams.py
+++ CH/OCR/C15 - Pangrams.py
@@ -1,23 +1,17 @@
 # Challenge 15 - Pangrams
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+letters = set("abcdefghijklmnopqrstuvwxyz")
 words = []
 
-check_pan = ""
-while check_pan != "stop":
-    check_pan = input("Enter a string. Type stop to continue: ")
-    if check_pan != "stop":
-        words.append(check_pan)
+while True:
+    check_pan = input("Enter a string. Type stop to continue: ").strip()
+    if check_pan.lower() == "stop":
+        break
+    words.append(check_pan)
 
-for pangram in words:
-    found_letters = []
-    sh = False
-    for let in pangram.lower():
-        if let in letters:
-            if let not in found_letters:
-                found_letters.append(let)
-                if len(found_letters) == 26:
-                    print(True)
-                    sh = True
-                    break
-    if not sh:
+for sentence in words:
+    found_letters = set(char for char in sentence.lower() if char in letters)
+
+    if found_letters == letters:
+        print(True)
+    else:
         print(False)
