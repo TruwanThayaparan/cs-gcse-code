f03265402ba6c455268220cb4cf9fa0f3ed9a983
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-06-03 18:13:40 +0100

    Update C03 - Thief!.py

--- CH/OCR/C03 - Thief!.py
+++ CH/OCR/C03 - Thief!.py
@@ -1,5 +1,32 @@
 # Challenge 3 - Thief!
 
+from itertools import permutations
+
+pin = "4214"  # set this to any 4-digit number
+
+while True:
+    dan = input("Input the four digits in question (XXXX): ").strip()
+    if len(dan) != 4 or not dan.isdigit():
+        print("You must enter exactly 4 digits.")
+        continue
+    break
+
+digits = list(dan)
+found = False
+
+for attempt_tuple in permutations(digits, 4):
+    attempt = ''.join(attempt_tuple)
+    print(attempt)
+    if attempt == pin:
+        print("PIN HAS BEEN CRACKED:", attempt)
+        found = True
+        break
+
+if not found:
+    print("PIN could not be cracked with these digits.")
+
+'''
+OLD VERSION:
 pin = "4214" # set this to any 4-digit number
 while True:
     dan = input("Input the four digits in question (XXXX): ").strip()
@@ -26,7 +53,7 @@ for i in range(4):
                 else:
                     print(attempt)
                 saved.append(attempt)
-
+'''
 
 # OLD VERSION (THIS DOES NOT ANSWER THE PROMPT CORRECTLY)
 '''
