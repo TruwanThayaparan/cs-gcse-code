52fe41ceb6676a7f23463683efcaa20ac02a1223
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2025-06-19 20:59:30 +0100

    better names

--- CH/Bebras/Counting Letters.py
+++ CH/Bebras/Counting Letters.py
@@ -1,12 +1,12 @@
-x = input().lower()
-a = 0
-b = 0
-for i in x:
+sentence = input().lower()
+vowels = 0
+consonants = 0
+for i in sentence:
   if i in set("aeiou"):
-    a += 1
+    vowels += 1
   elif i in set("bcdfghjklmnpqrstvwxyz"):
-    b += 1
+    consonants += 1
   else:
     pass
 
-print(a, b)
+print(vowels, consonants)
